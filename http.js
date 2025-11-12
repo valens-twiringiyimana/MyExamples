01df35dbb00f72a2762746d24cae0f380316f3ee
valens-twiringiyimana <60435245+valens-twiringiyimana@users.noreply.github.com>
2025-11-12 23:29:09 +0100

    My Commit please

--- http.js
+++ http.js
@@ -7,3 +7,11 @@ const options = {
   key: fs.readFileSync('server.key'),
   cert: fs.readFileSync('server.cert')
 };      
+https.createServer(options, (req, res) => {   
+  res.writeHead(200);
+  res.end('Hello Secure World\n');
+}).listen(443);
+console.log('Server welcome to my website please. Run the server my dear friend running at https://localhost:443/');
+
+
+
