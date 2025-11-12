ab8aa8cb06eb4a0b44ef91aef70c6c8cd42c8fa8
valens-twiringiyimana <60435245+valens-twiringiyimana@users.noreply.github.com>
2025-11-12 23:25:36 +0100

    Who are you baby

--- http.js
+++ http.js
@@ -1,3 +1,9 @@
 console('This is my server.');
 console.log('This is my server.');
 const https = require('https');
+const fs = require('fs');
+
+const options = {
+  key: fs.readFileSync('server.key'),
+  cert: fs.readFileSync('server.cert')
+};      
