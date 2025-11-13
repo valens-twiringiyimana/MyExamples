7549deb1d5848d8c2a632abd6d4d3633c0ab9aae
valens-twiringiyimana <60435245+valens-twiringiyimana@users.noreply.github.com>
2025-11-13 01:39:51 +0100

    MyAge

--- httpd.js
+++ httpd.js
@@ -5,3 +5,11 @@ let mama= `Dear friend, ${string} and  ${str3} `;
 console.log(mama);
 console.log( `Hello, ${str3} and ${mama}` );
 
+let isAGirl=true;
+let age=20;
+if (isAGirl && age >=18) {
+    console.log("She is eligible for scholarship.");
+
+} else {
+    console.log("She is not eligible for scholarship.");
+}
