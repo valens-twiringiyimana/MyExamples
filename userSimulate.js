948ea482c45172a560d9f1649ee1497538950822
valens-twiringiyimana <60435245+valens-twiringiyimana@users.noreply.github.com>
2025-11-13 22:58:46 +0100

    Attention

--- userSimulate.js
+++ userSimulate.js
@@ -1,4 +1,4 @@
-function longRunningOperation(callback) {
+/*function longRunningOperation(callback) {
   setTimeout(callback, 3000);}
     
 function userClicked(){
@@ -8,5 +8,35 @@ function userClicked(){
   }); 
   
 }
- userClicked();
+ userClicked();*/
 
+
+ function longRunningOperation(callback) {
+// simulate a 3 second operation
+setTimeout(callback, 3000);
+}
+function webRequest(request) {
+console.log('starting a long operation for request:', request.id);
+longRunningOperation(function () {
+console.log('ending a long operation for request:', request.id);
+});
+}
+// simulate a web request
+webRequest({ id: 1 });
+// simulate a second web request
+webRequest({ id: 2 });
+
+console.time('timer');
+setTimeout(function(){
+console.timeEnd('timer');
+},1000)
+
+console.time('timeit');
+function fibonacci(n) {
+if (n < 2)
+return 1;
+else
+return fibonacci(n - 2) + fibonacci(n - 1);
+}
+fibonacci(44); // modify this number based on your system performance
+console.timeEnd('timeit'); // On my system it takes about 9000ms (i.e. 9 seconds)
