d0a4b002f6fa7f990c887f8e246240ecbe390224
valens-twiringiyimana <60435245+valens-twiringiyimana@users.noreply.github.com>
2025-11-13 13:40:33 +0100

    Try again

--- httpd.js
+++ httpd.js
@@ -33,3 +33,4 @@ console.log(valens);
 valens.push("Grapes"); // adding an element to the end of the array
 valens.unshift("Bananas"); // adding an element to the beginning of the array
 console.log(valens);
+valens.pop(); // removing the last element of the array
