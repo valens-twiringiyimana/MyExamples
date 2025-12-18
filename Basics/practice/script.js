4caa9ea1355718a6a9ede678d4d2c1a994122a5b
valens-twiringiyimana <60435245+valens-twiringiyimana@users.noreply.github.com>
2025-12-18 12:15:28 +0100

    My update

---
+++ Basics/practice/script.js
@@ -0,0 +1,17 @@
+//Montly Income 
+let montlyIncome=3000;
+let rent=1200;
+let groceries=600;
+let utlities=300;
+let entertainment=200;
+
+
+//Total expenses
+
+let totalExpenses=rent+groceries+utlities+entertainment;
+
+let montlySavings=montlyIncome-totalExpenses;
+
+alert("Your montly savings are: $"+montlySavings);
+console.log("Total Expenses: $"+totalExpenses);
+
