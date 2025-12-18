4d226c63d3941751964737235e0455b7a15e98b8
valens-twiringiyimana <60435245+valens-twiringiyimana@users.noreply.github.com>
2025-12-18 15:13:06 +0100

    My strings

---
+++ Basics/java-script-basic/strings/script.js
@@ -0,0 +1,10 @@
+let firstName="Valens";
+let lastName="Mukandakaza";
+
+let fullName = firstName +" "+lastName;
+
+alert(fullName);
+
+
+let strLength = fullName.length;
+console.log("Length of Full Name: "+strLength);
